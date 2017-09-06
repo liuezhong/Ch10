db63839d9b1c0a3fac77997c2664a499d2a65e2c
Administrator <liuezhong@163.com>
2017-09-06 14:21:43 +0800

    K-均值聚类算法

--- mytry.py
+++ mytry.py
@@ -1,5 +1,16 @@
 import kMeans
 import numpy as np
+dataMat = np.mat(kMeans.loadDataSet('testSet.txt'))
+# print(dataMat)
+# print(dataMat[:,0].min())
+# print(kMeans.randCent(dataMat,2))
+# print(kMeans.distEclud(dataMat[0],dataMat[1]))
+
+myCentroids, clustAssing = kMeans.kMeans(dataMat,4)
+print(myCentroids)
+print("------------------")
+# print(clustAssing)
+
 # dataMat3 = np.mat(kMeans.loadDataSet('testSet2.txt'))
 # centList, myNewAssments = kMeans.biKmeans(dataMat3,3)
 
